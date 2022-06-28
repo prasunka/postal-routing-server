390229cf1d4673488a211d96709b64cf3a0af7ce
Prasun Kumar <prasun.code@gmail.com>
2022-06-28 10:46:36 +0530

    Added signature verification

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/prasunka/postal-routing-server/pkg/models/mysql"
 )
 
@@ -17,6 +20,26 @@ type Payload struct {
 	Forwardfrom string
 	Forwardto   string
 	Signature   string
+}
+
+func verifySig(from, sigHex string, msg []byte) bool {
+	sig, err := hexutil.Decode(sigHex)
+	if err != nil {
+		log.Println("Invalid signature!")
+		return false
+	}
+
+	msg = accounts.TextHash(msg)
+	sig[crypto.RecoveryIDOffset] -= 27 // Transform yellow paper V from 27/28 to 0/1
+
+	recovered, err := crypto.SigToPub(msg, sig)
+	if err != nil {
+		return false
+	}
+
+	recoveredAddr := crypto.PubkeyToAddress(*recovered)
+
+	return from == recoveredAddr.Hex()
 }
 
 func createRoute(w http.ResponseWriter, r *http.Request) {
@@ -34,16 +57,41 @@ func createRoute(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	routes := mysql.RouteModel{DB: DB}
-	endpoints := mysql.EndpointModel{DB: DB}
-
-	id, err := endpoints.Insert(payload.Forwardto)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	routes.Insert(id, payload.Forwardfrom)
-
 	fmt.Printf("%v\n", payload)
 
+	msg := []byte("I allow mails to be forwarded to " + payload.Forwardto)
+
+	if verifySig(payload.Forwardfrom, payload.Signature, msg) {
+		w.Header().Set("Content-Type", "application/json")
+		resp := make(map[string]string)
+
+		routes := mysql.RouteModel{DB: DB}
+		endpoints := mysql.EndpointModel{DB: DB}
+
+		id, err := endpoints.Insert(payload.Forwardto)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+
+		_, err = routes.Insert(id, payload.Forwardfrom)
+
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		resp["message"] = "Status Created"
+
+		jsonResp, err := json.Marshal(resp)
+		if err != nil {
+			log.Printf("Error happened in JSON marshal. Err: %s", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+
+		}
+
+		w.Write(jsonResp)
+
+	} else {
+		http.Error(w, "Signature verification failed. Begone intruder!", http.StatusUnauthorized)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/prasunka/postal-routing-server
 go 1.17
 
 require (
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.10.19 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/rs/cors v1.8.2 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 )
