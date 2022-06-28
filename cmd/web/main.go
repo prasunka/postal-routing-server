07f2cfccc32fc84ad52b5dd7edac2a1086ad983e
Prasun Kumar <prasun.code@gmail.com>
2022-06-28 10:54:27 +0530

    Cleanup: Removed redundant code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -57,35 +57,4 @@ func main() {
 	err = http.ListenAndServe(":4000", handler)
 
 	log.Fatal(err)
-
-	// stmt := `SELECT * FROM routes;`
-	// rows, err := db.Query(stmt)
-	// if err != nil {
-	// 	errorLog.Fatal(err)
-	// }
-	// defer rows.Close()
-
-	// for rows.Next() {
-	// 	var (
-	// 		id            int64
-	// 		uuid          string
-	// 		server_id     int64
-	// 		domain_id     int64
-	// 		endpoint_id   sql.NullInt64
-	// 		endpoint_type sql.NullString
-	// 		name          string
-	// 		spam_mode     string
-	// 		created_at    string
-	// 		updated_at    string
-	// 		token         string
-	// 		mode          string
-	// 	)
-	// 	if err := rows.Scan(&id, &uuid, &server_id, &domain_id, &endpoint_id, &endpoint_type, &name, &spam_mode, &created_at, &updated_at, &token, &mode); err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	log.Printf("id %d name is %s, %d, %d, %v, %v\n", id, uuid, server_id, domain_id, endpoint_id, endpoint_type)
-	// }
-	// if err != nil {
-	// 	errorLog.Fatal(err)
-	// }
 }
